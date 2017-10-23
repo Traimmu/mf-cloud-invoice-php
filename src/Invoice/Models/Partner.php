be83ac9215b88a3f274339d1bbd93f25b661db5d
gosho-kazuya <gosho-kazuya@919.jp>
2017-10-23 16:12:25 +0900

    collection get departments

--- src/Invoice/Models/Partner.php
+++ src/Invoice/Models/Partner.php
@@ -7,7 +7,12 @@ use Traimmu\MfCloud\Invoice\Models\Base;
 class Partner extends Base
 {
     protected $fields = [
-        'id', 'code', 'name', 'name_kana', 'name_suffix', 'memo',
+        'id', 'code', 'name', 'name_kana', 'name_suffix', 'memo', 'departments',
         'created_at', 'updated_at'
     ];
+
+    public function departments()
+    {
+        return collect($this['attributes']['departments']);
+    }
 }
